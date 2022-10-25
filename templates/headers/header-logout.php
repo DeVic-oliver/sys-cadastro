e3f07c552ca82bc9c40ad21c1ef872345d43842f
Victor Matheus <matheuslima.o183@gmail.com>
2022-10-25 13:21:53 -0300

    first commit

---
+++ templates/headers/header-logout.php
@@ -0,0 +1,8 @@
+<header class="system-header">
+    <div class="system-menu-container">
+        <nav class="logout-menu">
+            <a href="<?php echo ROOT; ?>" class="sign-in-item">Sign in</a>
+            <a href="<?php echo ROOT.'/signup.php'; ?>" class="sign-up-item">Sign up</a>
+        </nav>
+    </div>
+</header>
