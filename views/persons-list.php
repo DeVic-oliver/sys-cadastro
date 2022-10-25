6d7df79b662bfc890af0f63652c6313cc7005221
Victor Matheus <matheuslima.o183@gmail.com>
2022-10-25 16:50:33 -0300

    ROOT to DIRNAME

--- views/persons-list.php
+++ views/persons-list.php
@@ -1,3 +1,4 @@
+<?php include_once dirname(__DIR__) . '\config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +8,14 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include_once ROOT . '/templates/headers/header.php'; ?>
+    <?php include_once dirname(__DIR__) . '/templates/headers/header.php'; ?>
+    <?php include_once dirname(__DIR__) . '/templates/containers/main/main-system-open.html'; ?>
+    <div class="system-page-container">
+        <h1>Olá</h1>
+        <p>Você está na área interna do sistema! <br><br> Veja os outros integrantes do sistema!</p>
+        <h2>LISTAGEM</h2>
+    </div>
+    <?php include_once dirname(__DIR__) . '/templates/containers/main/main-system-close.html'; ?>
+
 </body>
 </html>
