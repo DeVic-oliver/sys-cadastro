8ace579db03861b18ba2fe1ce5419cec07ada2dd
Victor Matheus <matheuslima.o183@gmail.com>
2022-10-25 14:54:45 -0300

    get root repository

--- config.php
+++ config.php
@@ -1,2 +1,10 @@
 <?php
-define('ROOT', __FILE__);      
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+
+$uri .= $_SERVER['HTTP_HOST'];
+
+define('ROOT', $uri . '/sys-cadastro');  
