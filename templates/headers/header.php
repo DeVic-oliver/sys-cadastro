b8d0d1c4312bdae4e13eab62db2bed1aa062339c
Victor Matheus <matheuslima.o183@gmail.com>
2022-10-27 15:14:47 -0300

    file deleted

--- templates/headers/header.php
+++
@@ -1,8 +0,0 @@
-<header class="system-header">
-    <div class="system-main-menu-container">
-        <nav class="logged-menu">
-            <a href="" class="my-account-item">Minha conta</a>
-            <a href="" class="logout-item">Logout</a>
-        </nav>
-    </div>
-</header>
