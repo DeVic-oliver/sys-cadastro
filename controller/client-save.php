9b47bf6369995518f6bbadb910c328b65eb26253
Victor Matheus <victormatheus183@gmail.com>
2022-12-11 20:16:34 -0300

    useless files deleted

--- controller/client-save.php
+++
@@ -1,9 +0,0 @@
-<?php
-require dirname(__DIR__).'/model/databaseactions.php';
-require dirname(__DIR__).'/config.php';
-
-if(isset($_REQUEST) && !empty($_REQUEST)){
-    DatabaseActions::RegisterClient($_REQUEST);
-    header('Location: '.ROOT.'/views/thanks.html');
-    exit;
-}
