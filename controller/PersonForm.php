227bbec427a18eb820a3ad1f6707d09b74af5b6a
Victor Matheus <victormatheus183@gmail.com>
2022-12-11 21:23:30 -0300

    form controller created

---
+++ controller/PersonForm.php
@@ -0,0 +1,43 @@
+<?php
+
+ 
+
+namespace App\Controller;
+
+    use App\Model\Person;
+    use App\Model\City;
+
+    class PersonForm{
+        private $html;
+        private $data;
+
+        public function __construct(){
+            $this->html = file_get_contents( 'views/form.html' );
+            $this->data = array(
+                'id' => null,
+                'name' => null,
+                'address' => null,
+                'district' => null,
+                'phone' => null,
+                'email' => null,
+                'id_city' => null
+            );
+
+            $cities = '';
+            foreach(City::GetAllCities() as $city){
+                $cities .= "<option value='{$city['id']}>{$city['name']}</option>";
+            }
+
+            $this->html = str_replace('{cities}', $cities, $this->html);
+        }
+
+        public function editPerson($param){
+            try {
+                $id = (int) $param['id'];
+                $person = Person::findPerson($id);
+                $this->data = $person;
+            } catch (\Exception $e) {
+                print $e->getMessage();
+            }
+        }
+    }
